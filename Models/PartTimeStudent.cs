39a55acaccbfd7e77cb23cea5c97694badbf79da
Shakil <shakil.uddin@yahoo.com>
2020-12-10 19:54:19 +0600

    Hello

---
+++ Models/PartTimeStudent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_ERP.Models
+{
+    class PartTimeStudent : FullTimeStudents
+    {
+
+        public PartTimeStudent()
+        {}
+
+        public PartTimeStudent(string fname, string lname, string address, string citi, int tel, int t1, int t2,
+            int voter) : base(fname, lname, address, citi, tel, t1, t2, voter)
+        {}
+
+        public override string ComputeGrade()
+        {
+            double result = (0.40 + test_1) + (0.60 + test_2);
+            return result.ToString();
+        }
+    }
+}
---
+++ Models/PhDStudent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_ERP.Models
+{
+    class PhDStudent : FullTimeStudents
+    {
+        public PhDStudent()
+        {
+        }
+
+        public PhDStudent(string fname, string lname, string address, string citi, int tel, int t1, int t2,
+            int voter) : base(fname, lname, address, citi, tel, t1, t2, voter)
+        {
+
+        }
+
+
+        public override string ComputeGrade()
+        {
+            double result = (0.30 + test_1) + (0.70 + test_2);
+            return result.ToString();
+        }
+    }
+}
