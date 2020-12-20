d636a08955f67fe3526a7ef8103384938d8429b3
Shakil <shakil.uddin@yahoo.com>
2020-12-20 19:51:23 +0600

    Employee FullTimeFaculty PartTimeFaculty Model Added

---
+++ Models/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_ERP.Models
+{
+    class Employee
+    {
+        private int id;
+        private int empid;
+        private string firstname;
+        private string lastname;
+        private string streetaddress;
+        private string city;
+        private string department;
+        private int telephone;
+
+        public int Empid
+        {
+            get => empid;
+            set => empid = value;
+        }
+
+        public string Firstname
+        {
+            get => firstname;
+            set => firstname = value;
+        }
+
+        public string Lastname
+        {
+            get => lastname;
+            set => lastname = value;
+        }
+
+        public string Streetaddress
+        {
+            get => streetaddress;
+            set => streetaddress = value;
+        }
+
+        public string City
+        {
+            get => city;
+            set => city = value;
+        }
+
+        public string Department
+        {
+            get => department;
+            set => department = value;
+        }
+
+        public int Telephone
+        {
+            get => telephone;
+            set => telephone = value;
+        }
+    }
+}
---
+++ Models/PartTimeFaculty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_ERP.Models
+{
+    class PartTimeFaculty : Employee
+    {
+        private int id;
+        private int voterid;
+
+        public int Voterid
+        {
+            get => voterid;
+            set => voterid = value;
+        }
+    }
+}
